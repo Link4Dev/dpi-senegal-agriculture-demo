fdf4308432ebb972609af1d642c03cacb3ef5bc2
Link4Dev <60002010+Link4Dev@users.noreply.github.com>
2026-06-24 15:41:34 +0000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,7 @@ echo "🇸🇳 Initialisation de la démo OpenG2P pour le Ministère de l'Agricu
 sleep 15 
 
 # Exécution du script d'injection des filières Riz & Arachide
-python3 /workspace/data-seeds/setup_demo.py
+python3 /workspace/data-seeds/setup_demo.py &
 
 echo "✅ Environnement configuré avec succès !"
 echo "👉 Ouvrez l'onglet 'Ports' et cliquez sur l'adresse locale du port 8069 pour accéder à l'interface."
